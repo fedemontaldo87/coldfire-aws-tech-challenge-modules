97a7e9d16c96ef21673334688187bb57a6696402
fede650-art <federico@daas.consulting>
2025-07-20 21:24:57 -0300

    fix modules 2

--- terraform-aws-security-groups/outputs.tf
+++ terraform-aws-security-groups/outputs.tf
@@ -1,9 +1,3 @@
-output "asg_sg_id" {
-  description = "ID of the ASG security group"
-  value       = aws_security_group.asg_sg.id
-}
-
-output "alb_sg_id" {
-  description = "ID of the ALB security group"
-  value       = aws_security_group.alb_sg.id
+output "security_group_id" {
+  value = aws_security_group.this.id
 }
